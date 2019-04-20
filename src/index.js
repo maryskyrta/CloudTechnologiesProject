fb78aa957bc33748f1c6aa49adeacff061d75976
Daryna Oliinyk <darusya172@gmail.com>
2019-04-20 11:01:57 +0300

    begin page

--- src/index.js
+++ src/index.js
@@ -3,3 +3,15 @@ import './scss/main.scss';
 
 console.log('Hello!');
 console.log(`The time is ${new Date()}`);
+
+$(document).ready(function () {
+    $('#toRegistration').on('click', function () {
+        $('#registration').removeAttr("hidden");
+        $('#login').attr("hidden", "true");
+    });
+
+    $('#toLogin').on('click', function () {
+        $('#login').removeAttr("hidden");
+        $('#registration').attr("hidden", "true");
+    });
+});

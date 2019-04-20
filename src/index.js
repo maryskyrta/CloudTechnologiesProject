13c90127f90aa7623bb943cdadaeabefca48ee7b
Your Name <you@example.com>
2019-04-20 11:23:24 +0300

    Yulias commit

--- src/index.js
+++ src/index.js
@@ -1,5 +1,26 @@
 import 'bootstrap';
 import './scss/main.scss';
 
-console.log('Hello!');
-console.log(`The time is ${new Date()}`);
+    $(document).ready(function(){
+        // Activate tooltip
+        $('[data-toggle="tooltip"]').tooltip();
+
+        // Select/Deselect checkboxes
+        var checkbox = $('table tbody input[type="checkbox"]');
+        $("#selectAll").click(function(){
+            if(this.checked){
+                checkbox.each(function(){
+                    this.checked = true;
+                });
+            } else{
+                checkbox.each(function(){
+                    this.checked = false;
+                });
+            }
+        });
+        checkbox.click(function(){
+            if(!this.checked){
+                $("#selectAll").prop("checked", false);
+            }
+        });
+    });
